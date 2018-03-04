737cb9ffd82ef1ed7caf16c68e6f8049e47dfa24
Kleinman <ted.kleinman@hotmail.com>
2018-03-04 17:11:58 +0100

    Fixed callback events for keyboard, mouse and path drops.

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 
 #include "gl_debug.h"
 
-void framebuffer_resize(GLFWwindow* window, int width, int height)
+void framebuffer_resize_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
 }
@@ -17,6 +17,53 @@ void framebuffer_resize(GLFWwindow* window, int width, int height)
 void window_close_callback(GLFWwindow* window)
 {
     std::cout << "Closing the application." << std::endl;
+}
+
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+void character_callback(GLFWwindow* window, unsigned int codepoint)
+{
+    std::cout << "Character: " << codepoint << std::endl;
+}
+
+void charmods_callback(GLFWwindow* window, unsigned int codepoint, int mods)
+{
+    std::cout << "Character: " << codepoint << " with mods " << mods << std::endl;
+}
+
+static void cursor_position_callback(GLFWwindow* window, double x, double y)
+{
+    // std::cout << "Mouse position: " << x << ", " << y << std::endl;
+}
+
+void cursor_enter_callback(GLFWwindow* window, int entered)
+{
+    if (entered)
+        std::cout << "Welcome back mouse!" << std::endl;
+    else
+        std::cout << "Come back here, mouse!" << std::endl;
+}
+
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+{
+    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+        std::cout << "Clicked!" << std::endl;
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    std::cout << "Scrolling: " << xoffset << ", " << yoffset << std::endl;
+}
+
+void drop_callback(GLFWwindow* window, int count, const char** paths)
+{
+    for (int i = 0;  i < count;  i++)
+        std::cout << "Dropped file: " << paths[i] << std::endl;
 }
 
 int main()
@@ -34,9 +81,26 @@ int main()
     GLFWwindow* window = glfwCreateWindow(420, 420, "OpenGL Project", nullptr, nullptr);
 
     glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebuffer_resize);
+
+    // CALLBACKS - If you don't need to be notified via callbacks, all these can be queried through function calls.
+
+    // Window events
+    glfwSetFramebufferSizeCallback(window, framebuffer_resize_callback);
     glfwSetWindowCloseCallback(window, window_close_callback);
 
+    // Keyboard events
+    glfwSetKeyCallback(window, key_callback);
+    glfwSetCharCallback(window, character_callback);
+    glfwSetCharModsCallback(window, charmods_callback);
+
+    // Mouse events
+    glfwSetCursorPosCallback(window, cursor_position_callback);
+    glfwSetCursorEnterCallback(window, cursor_enter_callback);
+    glfwSetMouseButtonCallback(window, mouse_button_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+
+    // Path drop
+    glfwSetDropCallback(window, drop_callback);
 
     if (window == nullptr)
         throw std::runtime_error("Failed to create GLFW window.");
